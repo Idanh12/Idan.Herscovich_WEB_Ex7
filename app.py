18469b1c2252b99de60414a94b447f30f81e0623
idan.hershcovich <idanh12@gmail.com>
2021-12-08 19:46:59 +0200

    Ex_7

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return redirect('/about')
+
+@app.route('/about')
+def about_func():
+    return redirect(url_for('categories_func'))
+
+@app.route('/categories')
+def categories_func():
+    return 'Here you will see the categories'
 
 
 if __name__ == '__main__':

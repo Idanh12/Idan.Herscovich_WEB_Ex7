ef10924c465de96e37b07006f4c4d073bdb21445
idan.hershcovich <idanh12@gmail.com>
2021-12-29 13:49:36 +0200

    Ex_9

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
+from list import users
 
 app = Flask(__name__)
 
+app.secret_key = "abc"
 
 @app.route('/')
 def home_page():  # put application's code here
@@ -10,8 +12,37 @@ def home_page():  # put application's code here
 
 @app.route('/block')
 def about_func():
-    return render_template('block.html', name_of_class="web", degree='BSc', admin=True, Languages=['hebrew (native)', 'English (fluent)'])
+    return render_template('block.html', name_of_class="web", degree='BSc', admin=True,
+                           Languages=['Hebrew (native)', 'English (fluent)'])
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9_func():
+    if request.method == 'GET':
+        if "Search" in request.args:
+            search = request.args["Search"]
+            res = []
+            for user in users:
+                if user["first name"] == search or user["Last name"] == search:
+                    res.append(user)
+            if search == "":
+                res = users
+            if session.get('login'):
+                return render_template('assignment9.html', res=res, nickname=session.get('nick_name'))
+            else:
+                return render_template('assignment9.html', res=res)
+    if request.method == 'POST':
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        nick_name = request.form["nick_name"]
+        email = request.form["email"]
+        session['nick_name'] = nick_name
+        session['login'] = True
+    if session.get('login'):
+        return render_template('assignment9.html',
+                               nickname=session.get('nick_name'))
+    return render_template('assignment9.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ list.py
@@ -0,0 +1,6 @@
+users = [{'first name':'Yehuda', 'Last name': 'Levi'},
+         {'first name':'Yossi', 'Last name': 'Levi'},
+         {'first name':'Ehud', 'Last name': 'Banai'},
+         {'first name':'Yossi', 'Last name': 'Banai'},
+         {'first name':'Eviyatar', 'Last name': 'Banai'},
+         {'first name':'Orna', 'Last name': 'Banai'}]
